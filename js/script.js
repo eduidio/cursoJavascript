8da3ec17d6d035585df16688c7e7410a77283189
eduidio <eduidio@gmail.com>
2024-08-06 00:05:07 -0300

    agregando los demas ejercicios

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
- 
+/* calculadora*/ 
 function calculadora(num1,num2,operador){
     if(operador === "+"){
         return num1 + num2;
@@ -16,7 +16,23 @@ function calculadora(num1,num2,operador){
     return "operador incorrecto";
 }
 }
-console.log(calculadora(7,3,"+"));
 
 
+/*producto con descuento "el ejercicio consta en hacer un descuento del 20% en un local superando los $1000 de compra*/
 
+function ventaConDescuento(monto){
+    if (monto>1000){
+        return monto *0.8;
+    }else{
+        return monto;
+    }
+}
+
+//calcular ventas "el ejercicio se basa en calcular las ventas de 2 productos"//
+
+function ventas (productos1, productos2){
+    const precioProducto1= 1000;
+    const precioProducto2= 1500;
+
+    return productos1 * precioProducto1 + productos2 * precioProducto2; 
+}
